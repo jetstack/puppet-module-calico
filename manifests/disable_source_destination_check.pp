f30911ae1571c219b7c313edea4cca21be21e897
Matt Bates <bates@jetstack.io>
2017-11-11 22:33:58 +0000

    Inherit Calico params to obtain the region

--- manifests/disable_source_destination_check.pp
+++ manifests/disable_source_destination_check.pp
@@ -1,9 +1,8 @@
 # This class disable the source/destination check on AWS instances
-class calico::disable_source_destination_check{
-  include calico
+class calico::disable_source_destination_check() inherits ::calico::params
+{
+  include ::calico
   include ::kubernetes
-
-  $aws_region = $::calico::aws_region
 
   kubernetes::apply{'disable-srcdest-node':
     manifests => [
